ffb52aa03c49a5b0e413082eb76a865bd4a2aafd
Dan Louis Dela Cruz <louisdc002@gmail.com>
2023-03-10 20:25:53 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="icon.png" type="image/x-icon">
+  <title>Dashboard - DHVSU Finance Office</title>
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- JS -->
+  <script defer src="js/app.js"></script>
+
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+
+<body>
+  <header class="header">
+    <div class="brand">
+      <img src="icon.png" alt="" class="logo">
+      <div class="site-name">
+        <p class="school">DHVSU</p>
+        <p class="office">Finance Office</p>
+      </div>
+    </div>
+
+    <div class="header-icon">
+      <i class="bi bi-gear-fill"></i>
+    </div>
+
+    <div class="header-icon">
+      <i class="bi bi-bell-fill"></i>
+    </div>
+
+
+    <div class="header-user">
+      <div class="user-account">
+        <a href="" class="username">
+          <p>Juan Dela Cruz</p>
+        </a>
+        <a href="" class="logout">Logout</a>
+      </div>
+      <img src="assets/profile-placeholder.jpg" alt="" class="display-photo">
+    </div>
+  </header>
+</body>
+
+</html>
